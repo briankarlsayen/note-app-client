ee0b141d26548b5e5f7eabfb02d553ef683cf2ab
blu3fire89 <blu3fire89@gmail.com>
2022-07-25 21:23:17 +0800

    update

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     baseURL: "https://note-app-server-v1.herokuapp.com",
-    timeout: 2,
+    // timeout: 2,
     // baseURL: "http://localhost:5632",
 });
 
