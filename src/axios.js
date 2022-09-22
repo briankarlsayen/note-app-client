ca3327a5e4efdc0e58063337fdbb7043a1b5bdff
BrianSayen <blu3fire89@gmail.com>
2022-09-22 15:50:47 +0800

    update: revise backend server

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "https://note-app-server-v1.herokuapp.com",
+    baseURL: "https://note-app-server.onrender.com",
+    // baseURL: "https://note-app-server-v1.herokuapp.com",
     // timeout: 2,
     // baseURL: "http://localhost:5632",
 });
